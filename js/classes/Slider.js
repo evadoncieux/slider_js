b2ecf095af07995412f55d027d1d1282569ae75f
Eva Doncieux <eva.doncieux@gmail.com>
2023-10-19 15:42:54 +0200

    slider ok, added css files but not working

--- js/classes/Slider.js
+++ js/classes/Slider.js
@@ -2,7 +2,8 @@
 
 class Slider {
   constructor(params) {
-    this.images = params.images;
+    this.imageData = params.images;
+    // console.log(images)
     this.area = document.querySelector(params.area);
     this.currentIndex = 0;
 
@@ -10,7 +11,6 @@ class Slider {
     this.sliderHeight = params.height;
 
     this.createSlider();
-
 
   }
 
@@ -21,20 +21,39 @@ class Slider {
     this.slider.style.width = this.sliderWidth;
     this.slider.style.height = this.sliderHeight;
 
-    this.images = document.createElement('img');
+    this.imgElement = document.createElement('img');
+    this.imgElement.src = this.imageData[this.currentIndex].src;
+    this.slider.appendChild(this.imgElement);
+
+
     this.area.appendChild(this.slider);
 
     const prevButton = document.createElement('button');
+    prevButton.addEventListener('click', () => this.prevImage());
     this.area.appendChild(prevButton);
 
     const nextButton = document.createElement('button');
+    nextButton.addEventListener('click', () => this.nextImage());
     this.area.appendChild(nextButton);
-
-
 
     this.area.appendChild(this.slider);
 
   }
+
+  prevImage() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.imgElement.src = this.imageData[this.currentIndex].src;
+    }
+  }
+
+  nextImage() {
+    if (this.currentIndex < this.imageData.length - 1) {
+      this.currentIndex++;
+      this.imgElement.src = this.imageData[this.currentIndex].src;
+    }
+  }
+
 }
 
 export default Slider;
